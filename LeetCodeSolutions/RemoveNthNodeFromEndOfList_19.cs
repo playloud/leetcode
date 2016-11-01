ad8939a9f74d484112c4a896f8f9a30a6b6f271c
playloud78 <playloud78@gmail.com>
2016-10-31 23:49:42 -0400

    second commit

---
+++ LeetCodeSolutions/RemoveNthNodeFromEndOfList_19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+	public class RemoveNthNodeFromEndOfList_19
+	{
+		public RemoveNthNodeFromEndOfList_19()
+		{
+		}
+
+		public class Solution
+		{
+			public ListNode RemoveNthFromEnd(ListNode head, int n)
+			{
+
+
+
+				return null;
+			}
+		}
+
+		public class ListNode
+		{
+ 			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+ 		}
+	}
+}
